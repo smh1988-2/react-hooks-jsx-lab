479e961d25ece807946aa5de779c242fd8be7893
smh1988-2 <seamhurley1988@gmail.com>
2021-11-21 14:56:19 -0800

    commit tree 2614b560209605972787c4e48943d144c76e39ff

--- src/components/About.js
+++ src/components/About.js
@@ -1,8 +1,14 @@
 import React from "react";
-import { image } from "../data/data";
+import { image } from "../data/data.js";
 
 function About() {
-  return <div>About</div>;
+  return (
+  <div id="about">
+    <h2>About Me</h2>
+    <p>I am a human.</p>
+    <img src={ image } alt="I made this"></img>
+  </div>
+  );
 }
 
 export default About;
